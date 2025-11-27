7093f595b2c36b1200e985a8b8b83dab9861e9b5
Mugambi <dmaingi90@gmail.com>
2025-11-27 04:57:20 +0300

    added a couple of functions in index.js

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Example usage - calling the function:
+console.log(calculateTax(100));
+console.log(calculateTax(250));
+console.log(calculateTax(49.99));
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Example usage - calling the function:
+console.log(convertToUpperCase("hello"));
+console.log(convertToUpperCase("javascript"));
+console.log(convertToUpperCase("Hello World!"));
+
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+// Example usage - calling the function:
+console.log(findMaximum(10, 20));
+console.log(findMaximum(50, 25));
+console.log(findMaximum(7, 7));
+
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+// Example usage - calling the function:
+console.log(isPalindrome("racecar"));
+console.log(isPalindrome("hello"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("level"));
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discountAmount;
+}
+
+// Example usage - calling the function:
+console.log(calculateDiscountedPrice(100, 20));
+console.log(calculateDiscountedPrice(50, 10));
+console.log(calculateDiscountedPrice(200, 25));
 
 
 
